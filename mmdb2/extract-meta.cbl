4906d0e7cfb6645b2f06d84daef4fada8e4ff8e5
Marc Neudert <marc.neudert@gmail.com>
2016-08-27 12:04:47 +0200

    removes unneeded variables

--- mmdb2/extract-meta.cbl
+++ mmdb2/extract-meta.cbl
@@ -11,14 +11,6 @@ working-storage section.
 
 01 meta-offset  pic x(8) comp-x.
 
-01 needle-rec.
-   05 needle-char  pic x(1) comp-x.
-   05 needle-len   pic x(1) comp-x.
-   05 needle-str   pic x(14).
-
-01 search-rec      pic x.
-   88 search-done  value 'Y', false 'N'.
-
 linkage section.
 
 01 database  pic x(4).
