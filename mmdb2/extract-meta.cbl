961b020d6b300c676ab5d80f81bfefb626ce97de
Marc Neudert <marc.neudert@gmail.com>
2016-09-23 10:56:54 +0200

    displays first bitstring at meta start

--- mmdb2/extract-meta.cbl
+++ mmdb2/extract-meta.cbl
@@ -9,6 +9,10 @@ program-id. mmdb2-extract-meta.
 data division.
 working-storage section.
 
+01 buffer-rec.
+   05 buffer-byte       pic x(1).
+   05 buffer-bitstring  pic x(8).
+
 01 meta-offset  pic x(8) comp-x.
 
 linkage section.
@@ -20,5 +24,14 @@ linkage section.
 procedure division using database.
   call 'mmdb2-locate-meta' using database, meta-offset
 
-  display 'meta start position: ' meta-offset
+  perform read-buffer-byte
+
+  call 'binary-ctob' using buffer-byte, buffer-bitstring
+
+  display 'meta start:     ' meta-offset
+  display 'first bistring: ' buffer-bitstring
 exit program.
+
+read-buffer-byte.
+  call 'CBL_READ_FILE' using database, meta-offset, 1, 0, buffer-byte
+  .
