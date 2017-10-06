79502db3a978b53cbdf39c09bd815488bbc2d17c
Marc Neudert <marc.neudert@gmail.com>
2017-10-06 18:58:23 +0200

    fixes mmdb2 open/close data identifier

--- mmdb2/close.cbl
+++ mmdb2/close.cbl
@@ -9,12 +9,12 @@ program-id. mmdb2-close.
 data division.
 linkage section.
 
-01 handle  pic x(4).
+01 database  pic x(4).
 
 *>*********************************************************************
 
-procedure division using handle.
-  call 'CBL_CLOSE_FILE' using handle.
+procedure division using database.
+  call 'CBL_CLOSE_FILE' using database.
 
   if return-code <> 0
     display 'failed to open file (return code: ' return-code ')'
